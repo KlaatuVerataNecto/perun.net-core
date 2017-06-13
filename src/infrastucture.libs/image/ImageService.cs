15c7a3f695d11e1b7173b3d48649135b0608ddee
Artur Kedzior <artur@kedzior.es>
2017-06-14 01:39:22 +0200

    - added image upload

--- src/infrastucture.libs/image/ImageService.cs
+++ src/infrastucture.libs/image/ImageService.cs
@@ -1,23 +1,60 @@
-﻿//using ImageSharp;
-//using infrastucture.libs.cryptography;
+﻿using SkiaSharp;
 using System;
-//using System.Drawing;
-//using System.Drawing.Imaging;
 using System.IO;
 
 namespace infrastucture.libs.image
 {
-    public static class ImageService
+    public class ImageConfig
     {
-        public static void Save(FileStream fileStream, string pathToImages, Guid custom_id)
+        private string _sourcefilePath;
+        private string _savePathFile;
+        private int _quality;
+        private int _width;
+        private int _heigh;
+
+        public string SourceFilePath { get => _sourcefilePath; }
+        public string SavePathFile { get => _savePathFile; }
+
+        public ImageConfig CreateConfigFromImageFile(string sourcefilePath)
         {
-                //using (FileStream output = File.OpenWrite(@"C:\test.jpg"))
-                //using (Image<Rgba32> image = Image.Load<Rgba32>(fileStream))
-                //{
-                //    image.Resize(image.Width / 2, image.Height / 2)
-                //         .Grayscale()
-                //         .Save(output);
-                //}
+            this._sourcefilePath = sourcefilePath;
+            return this;
+        }
+        public ImageConfig WithSaveTo(string savePathFile)
+        {
+            this._savePathFile = savePathFile;
+            return this;
+        }
+    }
+
+    public static class ImageService 
+    {
+        // TODO: quality, resize params
+        public static string ResizeAndSave(ImageConfig config)
+        {
+            using (var input = File.OpenRead(config.SourceFilePath))
+            {
+                using (var inputStream = new SKManagedStream(input))
+                {
+                    using (var original = SKBitmap.Decode(inputStream))
+                    {
+                        using (var resized = original.Resize(new SKImageInfo(100, 100), SKBitmapResizeMethod.Lanczos3))
+                        {
+                            if (resized == null) return null;
+
+                            using (var image = SKImage.FromBitmap(resized))
+                            {
+                                using (var output = File.OpenWrite(config.SavePathFile))
+                                {
+                                    image.Encode(SKEncodedImageFormat.Jpeg, 90).SaveTo(output);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            return Path.GetFileName(config.SavePathFile);
         }
     }
 }
--- src/peruncore/Controllers/AvatarController.cs
+++ src/peruncore/Controllers/AvatarController.cs
@@ -12,12 +12,17 @@ namespace peruncore.Controllers
         public ActionResult upload(UserAvatarModel model)
         {
             var filePath = Path.GetTempFileName();
+
             using (var stream = new FileStream(filePath, FileMode.Create))
             {
                 model.avatar_image.CopyToAsync(stream);
-                ImageService.Save(stream, @"C:\", Guid.NewGuid());
             }
-            return Content("ok");
+            // TODO: read image file path from config
+            //Path.DirectorySeparatorChar
+            var config = new ImageConfig().CreateConfigFromImageFile(filePath).WithSaveTo(@"C:\github\perun.net.core\src\peruncore\wwwroot\images\test.jpg");
+            var image = ImageService.ResizeAndSave(config);
+
+            return File(config.SavePathFile, "image/jpeg");
         }
     }
 }
