e4ee1e0e1b472693a9e69a4c96c005867c90ea70
Klaatu Verata Necto <klaatuveratanecto@gmail.com>
2018-02-04 17:45:18 +0100

    - removed custom validations from all.min.js

--- src/peruncore/wwwroot/js/custom.validation.js
+++ src/peruncore/wwwroot/js/custom.validation.js
@@ -1,14 +1,18 @@
-﻿// Write your Javascript code.
-jQuery.validator.addMethod("nospaces", function (value, element) {
-    return value.indexOf(" ") < 0 && value != "";
-}, "Username has invalid characters. Spaces are not allowed.");
+﻿jQuery.validator.addMethod("nospaces", 
+    function (value, element) {
+        return value.indexOf(" ") < 0 && value != "";
+    },
+    "Username has invalid characters. Spaces are not allowed."
+);
 
-jQuery.validator.unobtrusive.adapters.add("nospaces", function (options) {
-    options.rules["nospaces"] = true;
-    if (options.message) {
-        options.messages["nospaces"] = options.message;
+jQuery.validator.unobtrusive.adapters.add("nospaces", 
+    function (options) {
+        options.rules["nospaces"] = true;
+        if (options.message) {
+            options.messages["nospaces"] = options.message;
+        }
     }
-});
+);
 
 jQuery.validator.addMethod("noweirdstuff", function (value, element) {
     return !(/\W/.test(value));
