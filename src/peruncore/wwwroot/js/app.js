d214ab53e844aa7feb50a76fc8b395ea6345dc3b
Klaatu Verata Necto <klaatuveratanecto@gmail.com>
2018-02-24 16:04:47 +0100

    - bundeled all css and all javascripts and removed external refences

--- src/peruncore/wwwroot/js/app.js
+++ src/peruncore/wwwroot/js/app.js
@@ -4,31 +4,6 @@
  */
 
 App = {
-    login: {
-        init: function () {
-            Validation.init();
-        }
-    },
-    signup: {
-        init: function () {
-            Validation.init();
-        }
-    },
-    username: {
-        init: function () {
-            Validation.init();
-        }
-    },
-    password: {
-        init: function () {
-            Validation.init();
-        }
-    },
-    account: {
-        init: function () {
-            Validation.init();
-        }
-    },
     post: {
         init: function () {
             Post.init();
--- src/peruncore/wwwroot/js/infrastructure/validations.js
+++ src/peruncore/wwwroot/js/infrastructure/validations.js
@@ -2,45 +2,30 @@
  * Validations
  * -------------------------------------------------------------------------
  */
-
 var Validation = function () {
 
     var handle = function () {
-        alert("addMethod");
-        jQuery.validator.addMethod("nospaces",
-            function (value, element) {
-                return value.indexOf(" ") < 0 && value != "";
-            },
-            "Username has invalid characters. Spaces are not allowed."
-        );
 
-        jQuery.validator.unobtrusive.adapters.add("nospaces",
-            function (options) {
-                options.rules["nospaces"] = true;
-                if (options.message) {
-                    options.messages["nospaces"] = options.message;
-                }
-            }
-        );
+        //$.validator.addMethod("nospaces",
+        //    function (value, element) { return value.indexOf(" ") < 0 && value != "";},"Username has invalid characters. Spaces are not allowed.");
 
-        jQuery.validator.addMethod("noweirdstuff", function (value, element) {
-            return !(/\W/.test(value));
-        }, "Username has invalid characters. Only letters, numbres & underscores allowed.");
+        //$.validator.unobtrusive.adapters.add("nospaces", function (options) {
+        //    options.rules["nospaces"] = true;
+        //    if (options.message) {options.messages["nospaces"] = options.message;}}
+        //);
 
-        jQuery.validator.unobtrusive.adapters.add("noweirdstuff", function (options) {
-            options.rules["noweirdstuff"] = true;
-            if (options.message) {
-                options.messages["noweirdstuff"] = options.message;
-            }
-        });
+        //$.validator.addMethod("noweirdstuff", function (value, element) {
+        //    return !(/\W/.test(value));}, "Username has invalid characters. Only letters, numbres & underscores allowed.");
 
-        jQuery.validator.unobtrusive.adapters.add("passwordmeter", function (options) {
-            options.rules["passwordmeter"] = true;
-            if (options.message) {
-                options.messages["passwordmeter"] = options.message;
-            }
-        });
+        //$.validator.unobtrusive.adapters.add("noweirdstuff", function (options) {
+        //    options.rules["noweirdstuff"] = true;
+        //    if (options.message) { options.messages["noweirdstuff"] = options.message;}
+        //});
 
+        //$.validator.unobtrusive.adapters.add("passwordmeter", function (options) {
+        //    options.rules["passwordmeter"] = true;
+        //    if (options.message) {  options.messages["passwordmeter"] = options.message;}
+        //});
     }
 
     return {
@@ -50,3 +35,26 @@ var Validation = function () {
     };
 
 }();
+
+$.validator.addMethod("nospaces",
+    function (value, element) { return value.indexOf(" ") < 0 && value != ""; }, "Username has invalid characters. Spaces are not allowed.");
+
+$.validator.unobtrusive.adapters.add("nospaces", function (options) {
+    options.rules["nospaces"] = true;
+    if (options.message) { options.messages["nospaces"] = options.message; }
+}
+);
+
+$.validator.addMethod("noweirdstuff", function (value, element) {
+    return !(/\W/.test(value));
+}, "Username has invalid characters. Only letters, numbres & underscores allowed.");
+
+$.validator.unobtrusive.adapters.add("noweirdstuff", function (options) {
+    options.rules["noweirdstuff"] = true;
+    if (options.message) { options.messages["noweirdstuff"] = options.message; }
+});
+
+$.validator.unobtrusive.adapters.add("passwordmeter", function (options) {
+    options.rules["passwordmeter"] = true;
+    if (options.message) { options.messages["passwordmeter"] = options.message; }
+});
